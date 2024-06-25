35aa676e2f24395c8a90352fc788c19516bea8b6
martijho <martinhovin91@gmail.com>
2024-06-25 08:59:55 +0200

    added variable types to botsortparams dataclass

--- tracker/bot_sort_params.py
+++ tracker/bot_sort_params.py
@@ -1,23 +1,19 @@
 from dataclasses import dataclass
 
+
 @dataclass
 class BoTSORTParams:
-    name = ""
-    with_reid = False
-    ablation = ""
-    proximity_thresh = 0.5  # Min IOU for matching
-    appearance_thresh = 0.25
-
-    track_low_thresh = 0.1  # Confidence threshold
-    track_high_thresh = 0.3
-    new_track_thresh = 0.6 #track_high_thresh + 0.1
-
-    track_buffer = 15
-    match_thresh = 0.8
-    aspect_ratio_thresh = 1.6
-    fuse_score = False
-
-    # cmc_method = "orb"
-    cmc_method = "None"
-    mot20 = False
-
+    name: str = ""
+    with_reid: bool = False
+    ablation: str = ""
+    proximity_thresh: float = 0.5  # Min IOU for matching
+    appearance_thresh: float = 0.25
+    track_low_thresh: float = 0.1  # Confidence threshold
+    track_high_thresh: float = 0.3
+    new_track_thresh: float = 0.6  # track_high_thresh + 0.1
+    track_buffer: int = 15
+    match_thresh: float = 0.8
+    aspect_ratio_thresh: float = 1.6
+    fuse_score: bool = False
+    cmc_method: str = "None"
+    mot20: bool = False
